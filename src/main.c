415d1b665e7c2f6b6af1a3eb7825d5405be88f0e
no <no>
2024-10-25 00:37:54 +0000

    renders

--- src/main.c
+++ src/main.c
@@ -67,6 +67,30 @@ int main() {
         return RETURN_ERROR;
     }
 
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f,  // top left
+        0.0f,  0.75f, 0.0f,  // tip
+    };
+
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), NULL);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
     // Render loop
     while (!glfwWindowShouldClose(window))
     {
@@ -76,13 +100,19 @@ int main() {
         // Render
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 5);
+
         // Update
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     // Clean up
-    // ...
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     // Terminate
     glfwTerminate();
