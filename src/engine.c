d7c5a126d5ecb0ba39d899a8c885d6a963e0c254
no <no>
2024-11-11 22:15:37 +0000

    added TODO

--- src/engine.c
+++ src/engine.c
@@ -6,6 +6,8 @@
 #include <box2d/box2d.h>
 #include <stdio.h>
 
+// TODO: figure out how to return a rendered frame to python (as a type of observation, or to just let python handle window management)
+
 #define TIME_STEP   1.0f / 60.0f
 #define SUB_STEPS   4.0f
 
