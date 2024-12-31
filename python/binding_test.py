6dd5819923356c4cf9df1935ce7b79cb57ab97b8
no <no>
2024-12-31 00:34:28 +0000

    added python loading test, dummy binding

---
+++ python/binding_test.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+# Add the build directory to the sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../build')))
+
+import tank_game
+
+print(dir(tank_game))
--- src/binding.cpp
+++ src/binding.cpp
@@ -2,4 +2,7 @@
 
 namespace py = pybind11;
 
-
+PYBIND11_MODULE(tank_game, handle) {
+    handle.doc() = "Tank Game Environment";
+    handle.def("test", []() { return "Hello, World!"; });
+}
