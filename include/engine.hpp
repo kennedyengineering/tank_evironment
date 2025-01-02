fdb60c473056fd75a8222196f1f6739153b31974
no <no>
2025-01-02 07:41:43 +0000

    initial work in oop

---
+++ include/engine.hpp
@@ -0,0 +1,16 @@
+// Tank Game (@kennedyengineering)
+
+#pragma once
+
+namespace TankGame
+{
+    class Engine
+    {
+    public:
+        Engine();
+        ~Engine();
+
+        void step();
+        void destroy();
+    };
+}
---
+++ src/engine.cpp
@@ -0,0 +1,25 @@
+// Tank Game (@kennedyengineering)
+
+#include "engine.hpp"
+
+using namespace TankGame;
+
+Engine::Engine()
+{
+    // Initialize the engine
+}
+
+Engine::~Engine()
+{
+    // Destroy the engine
+}
+
+void Engine::step()
+{
+    // Step the engine forward
+}
+
+void Engine::destroy()
+{
+    // Destroy the engine
+}
