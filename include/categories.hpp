245c3174458747cb886a25243fe7e36d81f460bf
no <no>
2025-01-04 00:28:10 +0000

    modified categories.hpp, only one TANK category and added ALL category

--- include/categories.hpp
+++ include/categories.hpp
@@ -2,14 +2,13 @@
 
 #pragma once
 
-// TODO: figure out how to make this scalable with more than two tanks. Maybe use new mechanism for collision resolution?
 namespace TankGame
 {
     enum CategoryBits
     {
-        PROJECTILE = 0x00000008,
-        WALL       = 0x00000004,
-        TANK1      = 0x00000002,
-        TANK2      = 0x00000001,
+        PROJECTILE = 0x00000004,
+        WALL       = 0x00000002,
+        TANK       = 0x00000001,
+        ALL        = 0xFFFFFFFF
     };
 }
