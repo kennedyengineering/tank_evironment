a578307a908bf3f2048789eaf199f7c30ac61dbb
no <no>
2025-01-05 06:58:49 +0000

    basic renderEngine functionality

--- include/render.hpp
+++ include/render.hpp
@@ -3,4 +3,20 @@
 #pragma once
 
 #include <cairo.h>
+#include <filesystem>
 
+namespace TankGame
+{
+    class RenderEngine
+    {
+    public:
+        RenderEngine(int imageWidth, int imageHeight);
+        ~RenderEngine();
+
+        void writeToPng(const std::filesystem::path& filePath);
+
+    private:
+        cairo_surface_t *mSurface;
+        cairo_t *mContext;
+    };
+}
--- src/render.cpp
+++ src/render.cpp
@@ -1,4 +1,43 @@
 // Tank Game (@kennedyengineering)
 
+#include <stdexcept>
+
 #include "render.hpp"
 
+using namespace TankGame;
+
+RenderEngine::RenderEngine(int imageWidth, int imageHeight)
+{
+    /* Construct the render engine */
+    
+    // Allocate the image surface
+    mSurface = cairo_image_surface_create(CAIRO_FORMAT_RGB24, imageWidth, imageHeight);
+    
+    // Create render context
+    mContext = cairo_create(mSurface);
+}
+
+void RenderEngine::writeToPng(const std::filesystem::path& filePath)
+{
+    /* Write the current image surface to disk */
+
+    // Check if the file has a ".png" extension
+    if (filePath.extension() != ".png")
+    {
+        throw std::invalid_argument("The file must be a .png");
+    }
+
+    // Save to disk
+    cairo_surface_write_to_png(mSurface, filePath.c_str());
+}
+
+RenderEngine::~RenderEngine()
+{
+    /* Destroy the render engine */
+
+    // Destroy render context
+    cairo_destroy(mContext);
+
+    // Deallocate the image surface
+    cairo_surface_destroy(mSurface);
+}
