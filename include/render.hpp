bd56faf55d6453a426b5c3ce346f0e86f5a71c88
no <no>
2025-01-08 18:27:06 +0000

    implemented several render methods

--- include/render.hpp
+++ include/render.hpp
@@ -4,14 +4,37 @@
 
 #include <cairo.h>
 #include <filesystem>
+#include <vector>
 
 namespace TankGame
 {
+    struct Point
+    {
+        float x, y;
+    };
+
+    struct Color
+    {
+        float r, g, b;
+    };
+
     class RenderEngine
     {
     public:
         RenderEngine(int imageWidth, int imageHeight);
         ~RenderEngine();
+        
+        // TODO: render tank
+        // TODO: render projectiles (separate methods, don't need debug draw since all shapes are being manually tracked. shape in list -> get body -> get world transform. shape -> get polygon -> vertices && count)
+        // TODO: render lidar scan
+        // TODO: implement the above methods in engine, have engine keep track of pixel density etc... initialize this class in initialization list
+
+        // TODO: get pixel buffer
+
+        void clearImage(Color color);
+
+        void renderPolygon(std::vector<Point> vertices, Color color);
+        void renderCircle(Point center, float radius, Color color);
 
         void writeToPng(const std::filesystem::path& filePath);
 
--- src/render.cpp
+++ src/render.cpp
@@ -1,6 +1,7 @@
 // Tank Game (@kennedyengineering)
 
 #include <stdexcept>
+#include <cmath>
 
 #include "render.hpp"
 
@@ -15,6 +16,66 @@ RenderEngine::RenderEngine(int imageWidth, int imageHeight)
     
     // Create render context
     mContext = cairo_create(mSurface);
+}
+
+void RenderEngine::clearImage(Color color)
+{
+    /* Clear the surface */
+
+    // Set color
+    cairo_set_source_rgb(mContext, color.r, color.g, color.b);
+
+    // Clear surface
+    cairo_paint(mContext);
+}
+
+void RenderEngine::renderPolygon(std::vector<Point> vertices, Color color)
+{
+    /* Render a polygon */
+
+    // Check input
+    if (vertices.size() < 3)
+    {
+        throw std::invalid_argument("Vector must have at least 3 entries.");
+    }
+
+    // Set color
+    cairo_set_source_rgb(mContext, color.r, color.g, color.b);
+
+    // Create path
+    bool first = true;
+
+    for (const Point& point : vertices)
+    {
+        if (first)
+        {
+            cairo_move_to(mContext, point.x, point.y);
+            first = false;
+        }
+        else
+        {
+            cairo_line_to(mContext, point.x, point.y);
+        }
+    }
+
+    cairo_close_path(mContext);
+
+    // Fill color
+    cairo_fill(mContext);
+}
+
+void RenderEngine::renderCircle(Point center, float radius, Color color)
+{
+    /* Render a circle */
+
+    // Set color
+    cairo_set_source_rgb(mContext, color.r, color.g, color.b);
+
+    // Create path
+    cairo_arc(mContext, center.x, center.y, radius, 0, 2.0f*M_PIf);
+
+    // Fill color
+    cairo_fill(mContext);
 }
 
 void RenderEngine::writeToPng(const std::filesystem::path& filePath)
