c65db4c85031bbaa0f9348c522db9b6bbe0c60db
no <no>
2025-01-08 20:39:55 +0000

    removed TODO from render.hpp

--- include/render.hpp
+++ include/render.hpp
@@ -16,8 +16,6 @@ namespace TankGame
         RenderEngine(int imageWidth, int imageHeight);
         ~RenderEngine();
         
-        // TODO: render tank
-        // TODO: render lidar scan
         // TODO: get pixel buffer (either create struct to hold vector (buffer) and image dimensions, or create another method to get image dimensions and call that in bindings lambda. probably the latter.)
 
         void clearImage(b2HexColor color);
