77c4e5d411f3b6d2260bb31ab9b065830cb8261f
no <no>
2025-01-09 04:47:03 +0000

    added placeholder tests

---
+++ test/test_registry.cpp
@@ -0,0 +1,11 @@
+// Tank Game (@kennedyengineering)
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(RegistryTest, BasicAssertions) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
