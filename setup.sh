733a4b17c3f1c0aee1bafa7a0d862d9c9f9726ec
no <no>
2025-01-09 05:53:34 +0000

    added python packages, precommit

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Constants
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# Setup environment
+echo -e "${GREEN}Creating virtual environment${NC}"
+python3 -m venv .venv
+source .venv/bin/activate
+
+echo -e "${GREEN}Installing packages${NC}"
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo -e "${GREEN}Installing pre-commit hooks${NC}"
+pre-commit install
+
+echo -e "${GREEN}Finished${NC}"
