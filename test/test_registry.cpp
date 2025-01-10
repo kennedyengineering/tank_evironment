c636439a0cc9cdcebe89e6804d7c0b24f463a030
no <no>
2025-01-10 23:13:52 +0000

    added complex types to registry test

--- test/test_registry.cpp
+++ test/test_registry.cpp
@@ -79,6 +79,141 @@ TEST(RegistryTest, PrimitiveTypeEmplaceWithId) {
   reg.remove(id2);
 }
 
-TEST(RegistryTest, ComplexType) {
-  // Ensure registry constructs complex data types
+class TestType {
+public:
+  TestType(int val) : mVal(val) { mConstructorCount++; }
+
+  ~TestType() { mDestructorCount++; }
+
+  int getVal() { return mVal; }
+
+  void setVal(int val) { mVal = val; }
+
+  static int getConstructorCount() { return mConstructorCount; }
+
+  static int getDestructorCount() { return mDestructorCount; }
+
+  static void resetCounters() {
+    mConstructorCount = 0;
+    mDestructorCount = 0;
+  }
+
+private:
+  int mVal;
+  static int mConstructorCount;
+  static int mDestructorCount;
+};
+
+int TestType::mConstructorCount = 0;
+int TestType::mDestructorCount = 0;
+
+TEST(RegistryTest, ComplexTypeInitialization) {
+  // Ensure registry works with complex data types
+
+  // Reset counters
+  TestType::resetCounters();
+
+  // Construct with TestType type
+  TankGame::Registry<TestType> reg;
+
+  // Ensure ids are checked
+  ASSERT_ANY_THROW(reg.get(0));
+  ASSERT_ANY_THROW(reg.remove(0));
+
+  // Check constructor count
+  ASSERT_EQ(TestType::getConstructorCount(), 0);
+
+  // Check destructor count
+  ASSERT_EQ(TestType::getDestructorCount(), 0);
+}
+
+TEST(RegistryTest, ComplexTypeEmplaceGetRemove) {
+  // Ensure registry works with complex data types
+
+  // Reset counters
+  TestType::resetCounters();
+
+  // Construct with TestType type
+  TankGame::Registry<TestType> reg;
+
+  // Emplace TestType
+  TankGame::RegistryId id = reg.emplace(2);
+
+  // Retrieve TestType value
+  ASSERT_EQ(reg.get(id).getVal(), 2);
+
+  // Modify TestType value
+  TestType &val = reg.get(id);
+  val.setVal(3);
+  ASSERT_EQ(reg.get(id).getVal(), 3);
+
+  // Delete TestType
+  reg.remove(id);
+
+  // Check constructor count
+  ASSERT_EQ(TestType::getConstructorCount(), 1);
+
+  // Check destructor count
+  ASSERT_EQ(TestType::getDestructorCount(), 1);
+}
+
+TEST(RegistryTest, ComplexTypeCountIds) {
+  // Ensure registry works with complex data types
+
+  // Reset counters
+  TestType::resetCounters();
+
+  // Construct with TestType type
+  TankGame::Registry<TestType> reg;
+
+  // Count Ids
+  TankGame::RegistryId id1 = reg.emplace(1);
+  ASSERT_EQ(id1, 0);
+  TankGame::RegistryId id2 = reg.emplace(2);
+  ASSERT_EQ(id2, 1);
+
+  reg.remove(id1);
+
+  TankGame::RegistryId id3 = reg.emplace(3);
+  ASSERT_EQ(id3, 0);
+
+  ASSERT_EQ(reg.get(0).getVal(), 3);
+  ASSERT_EQ(reg.get(1).getVal(), 2);
+
+  reg.remove(id2);
+  reg.remove(id3);
+
+  // Check constructor count
+  ASSERT_EQ(TestType::getConstructorCount(), 3);
+
+  // Check destructor count
+  ASSERT_EQ(TestType::getDestructorCount(), 3);
+}
+
+TEST(RegistryTest, ComplexTypeEmplaceWithId) {
+  // Ensure registry works with complex data types
+
+  // Reset counters
+  TestType::resetCounters();
+
+  // Construct with TestType type
+  TankGame::Registry<TestType> reg;
+
+  // Emplace char with id
+  TankGame::RegistryId id1 = reg.emplaceWithId();
+  ASSERT_EQ(reg.get(id1).getVal(), 0);
+  ASSERT_EQ(id1, 0);
+
+  TankGame::RegistryId id2 = reg.emplaceWithId();
+  ASSERT_EQ(reg.get(id2).getVal(), 1);
+  ASSERT_EQ(id2, 1);
+
+  reg.remove(id1);
+  reg.remove(id2);
+
+  // Check constructor count
+  ASSERT_EQ(TestType::getConstructorCount(), 2);
+
+  // Check destructor count
+  ASSERT_EQ(TestType::getDestructorCount(), 2);
 }
