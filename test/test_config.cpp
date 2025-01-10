7d08dfd279d254868c29584734f49da5b07dd9e1
no <no>
2025-01-10 21:16:32 +0000

    added config tests and TODO

--- test/test_config.cpp
+++ test/test_config.cpp
@@ -2,10 +2,18 @@
 
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(ConfigTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+#include "config.hpp"
+
+TEST(ConfigTest, TankConfigInitialization) {
+  // Ensure default initialization works
+  TankGame::TankConfig tankConfig;
 }
+
+// TODO: add TankConfig::ValidateConfig
+
+TEST(ConfigTest, ConfigInitialization) {
+  // Ensure default initialization works
+  TankGame::Config config;
+}
+
+// TODO: add Config::ValidateConfig
