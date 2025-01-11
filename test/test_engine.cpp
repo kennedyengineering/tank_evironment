cc61adca84b6e5ccb5431a49873720e542e48de0
no <no>
2025-01-11 04:31:29 +0000

    added other tank to lidar test

--- test/test_engine.cpp
+++ test/test_engine.cpp
@@ -49,27 +49,38 @@ TEST(EngineTest, RenderTankLidarToPng) {
   TankGame::Engine eng(config);
 
   // Create tank config
-  TankGame::TankConfig tankConfig;
-  tankConfig.positionX = config.arenaWidth / 2.0f;
-  tankConfig.positionY = config.arenaHeight / 2.0f;
+  TankGame::TankConfig tankConfig1;
+  tankConfig1.positionX = config.arenaWidth / 3.0f;
+  tankConfig1.positionY = config.arenaHeight / 2.0f;
 
   // Create tank
-  TankGame::RegistryId id = eng.addTank(tankConfig);
+  TankGame::RegistryId id1 = eng.addTank(tankConfig1);
+
+  // Create other tank config
+  TankGame::TankConfig tankConfig2;
+  tankConfig2.positionX = 2.0f * config.arenaWidth / 3.0f;
+  tankConfig2.positionY = config.arenaHeight / 2.0f;
+
+  // Create other tank
+  TankGame::RegistryId id2 = eng.addTank(tankConfig2);
 
   // Render tank
-  eng.renderTank(id);
+  eng.renderTank(id1);
+
+  // Render other tank
+  eng.renderTank(id2);
 
   // Render lidar
-  eng.renderTankLidar(id);
+  eng.renderTankLidar(id1);
 
   // Save to PNG
   eng.writeImageToPng("EngineTest_RenderTankLidarToPng_PreScan.png");
 
   // Scan lidar
-  eng.scanTankLidar(id);
+  eng.scanTankLidar(id1);
 
   // Render lidar
-  eng.renderTankLidar(id);
+  eng.renderTankLidar(id1);
 
   // Save to PNG
   eng.writeImageToPng("EngineTest_RenderTankLidarToPng_PostScan.png");
