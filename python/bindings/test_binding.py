f0e21a0ede8a0760d5128643351ff684ce327d5a
no <no>
2025-01-27 22:45:55 +0000

    added engine destructor binding test

--- python/bindings/test_binding.py
+++ python/bindings/test_binding.py
@@ -1,11 +1,13 @@
 # Tank Game (@kennedyengineering)
 
-import python_bindings
+import python_bindings as tank_game
 
 import pytest
 
 
 class TestBinding:
-    def test_one(self):
-        x = "this"
-        assert "h" in x
+    def test_engine_destroy(self):
+        config = tank_game.Config()
+
+        for i in range(100000):
+            engine = tank_game.Engine(config)
