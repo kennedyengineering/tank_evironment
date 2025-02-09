b59c77398163bca12796108d191d8b30164ad84f
no <no>
2025-02-09 05:15:13 +0000

    updated test fixture name

--- python/tank_game_environment/test/test_environment.py
+++ python/tank_game_environment/test/test_environment.py
@@ -15,22 +15,22 @@ import pytest
 
 
 class TestParallelEnvironment:
-    def test_parallel_api(self):
+    def test_pettingzoo_parallel_api(self):
         parallel_api_test(tank_game_environment_v0.parallel_env_fn())
 
-    def test_parallel_seed(self):
+    def test_pettingzoo_parallel_seed(self):
         parallel_seed_test(tank_game_environment_v0.parallel_env_fn)
 
 
 class TestAECEnvironment:
-    def test_api(self):
+    def test_pettingzoo_api(self):
         api_test(tank_game_environment_v0.aec_env_fn())
 
-    def test_seed(self):
+    def test_pettingzoo_seed(self):
         seed_test(tank_game_environment_v0.aec_env_fn)
 
-    def test_render(self):
+    def test_pettingzoo_render(self):
         render_test(tank_game_environment_v0.aec_env_fn)
 
-    def test_performance(self):
+    def test_pettingzoo_performance(self):
         performance_benchmark(tank_game_environment_v0.aec_env_fn())
