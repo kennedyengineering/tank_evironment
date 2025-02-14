c2f40b43add6627aa821e3bce6d069679f9a08e3
no <no>
2025-02-14 20:01:22 +0000

    wrapper passes all tests

---
+++ python/tank_game_environment/test/test_wrapper.py
@@ -0,0 +1,28 @@
+# Tank Game (@kennedyengineering)
+
+from .. import tank_game_environment_v0
+
+from ..wrapper.wrapper_agent import AgentWrapper
+from ..agent.agent_random import RandomAgent
+
+from gymnasium.utils.env_checker import check_env
+
+import pytest
+
+
+class TestAgentWrapper:
+    def test_gymnasium_check_env(self):
+
+        env = tank_game_environment_v0.parallel_env_fn()
+
+        assert len(env.possible_agents) == 2
+        learning_agent = env.possible_agents[0]
+        scripted_agent = env.possible_agents[1]
+
+        scripted_policy = RandomAgent(
+            env.observation_space(scripted_agent), env.action_space(scripted_agent)
+        )
+
+        env = AgentWrapper(env, learning_agent, scripted_agent, scripted_policy)
+
+        check_env(env)
---
+++ python/tank_game_environment/wrapper/wrapper_agent.py
@@ -0,0 +1,118 @@
+# Tank Game (@kennedyengineering)
+
+from ..agent.agent_base import ScriptedAgent
+
+from pettingzoo import ParallelEnv
+from gymnasium import Env
+
+
+class AgentWrapper(Env):
+    """
+    A Gymnasium wrapper that turns a two-agent PettingZoo Parallel environment into a single-agent
+    environment by replacing one agent with a scripted policy.
+
+    Args:
+        pet_env: The PettingZoo multi-agent environment (Parallel-style).
+        learning_agent: The agent that will be controlled by your RL algorithm.
+        scripted_agent: The agent that will be controlled by a scripted policy.
+        scripted_policy: A function that takes an observation (for the scripted agent) and returns an action.
+    """
+
+    def __init__(
+        self,
+        parallel_env: ParallelEnv,
+        learning_agent: str,
+        scripted_agent: str,
+        scripted_policy: ScriptedAgent,
+    ):
+        super().__init__()
+
+        if not isinstance(parallel_env, ParallelEnv):
+            raise TypeError(
+                f"Expected parallel_env to be pettingzoo Parallel, got {type(parallel_env)}"
+            )
+        self._env = parallel_env
+
+        if not isinstance(learning_agent, str):
+            raise TypeError(
+                f"Expected learning_agent to be str, got {type(parallel_env)}"
+            )
+        if learning_agent not in parallel_env.possible_agents:
+            raise ValueError(
+                f"Value of learning_agent is not a valid agent id, passed {learning_agent}"
+            )
+        self._learning_agent = learning_agent
+
+        if not isinstance(scripted_agent, str):
+            raise TypeError(
+                f"Expected scripted_agent to be str, got {type(parallel_env)}"
+            )
+        if scripted_agent not in parallel_env.possible_agents:
+            raise ValueError(
+                f"Value of scripted_agent is not a valid agent id, passed {scripted_agent}"
+            )
+        self._scripted_agent = scripted_agent
+
+        if not isinstance(scripted_policy, ScriptedAgent):
+            raise TypeError(
+                f"Expected scripted_policy to be tank_game_environment ScriptedAgent, got {type(scripted_policy)}"
+            )
+        self._scripted_policy = scripted_policy
+
+        self._scripted_observation = None
+
+        self.observation_space = self._env.observation_space(learning_agent)
+        self.action_space = self._env.action_space(learning_agent)
+
+    def reset(self, seed=None, options=None):
+        """
+        Reset the underlying PettingZoo environment.
+        """
+        super().reset(seed=seed)
+        self._scripted_policy.action_space.seed(seed=seed)
+
+        observations, infos = self._env.reset(seed, options)
+
+        self._scripted_observation = observations[self._scripted_agent]
+
+        return observations[self._learning_agent], infos[self._learning_agent]
+
+    def step(self, action):
+        """
+        Execute one step in the environment.
+        """
+
+        actions = {
+            self._learning_agent: action,
+            self._scripted_agent: self._scripted_policy.policy(
+                self._scripted_observation
+            ),
+        }
+
+        observations, rewards, terminations, truncations, infos = self._env.step(
+            actions
+        )
+
+        self._scripted_observation = observations[self._scripted_agent]
+
+        return (
+            observations[self._learning_agent],
+            rewards[self._learning_agent],
+            terminations[self._learning_agent],
+            truncations[self._learning_agent],
+            infos[self._learning_agent],
+        )
+
+    def render(self):
+        """
+        Render the environment.
+        """
+
+        return self._env.render()
+
+    def close(self):
+        """
+        Uninitialize the environment.
+        """
+
+        return self._env.close()
