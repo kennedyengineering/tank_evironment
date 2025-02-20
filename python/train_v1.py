b3561e024546613cdac5d0da944b51a6b6b948b4
no <no>
2025-02-20 04:33:31 +0000

    removed TODO

--- python/train_v1.py
+++ python/train_v1.py
@@ -21,8 +21,6 @@ from stable_baselines3.common.callbacks import (
     EvalCallback,
 )
 
-# TODO: handle determinism / seeds. does there need to be a different seed for train_env and eval_env? (set eval_env to seed, then train_env to seed + 1)
-
 
 def train():
     """Train an agent."""
