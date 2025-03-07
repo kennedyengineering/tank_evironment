11c0a9b3718d6a37ebb8f5afbc5e47beb768a2f4
Ubuntu <ubuntu@TOOTHLESS.>
2025-03-07 23:30:09 +0000

    removed unused import

--- python/train_v2.py
+++ python/train_v2.py
@@ -19,7 +19,6 @@ import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.ppo import MlpPolicy
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import (
     CallbackList,
