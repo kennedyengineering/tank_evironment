c2bf973711bbb10eb2bfe85f98d316a970afde06
Ubuntu <ubuntu@TOOTHLESS.>
2025-03-10 22:09:01 +0000

    increased step count to 1M, deleted TODO msgs

--- python/train_v0.py
+++ python/train_v0.py
@@ -12,11 +12,8 @@ from supersuit import frame_stack_v1
 from stable_baselines3 import PPO
 from stable_baselines3.ppo import MlpPolicy
 
-# TODO: log videos to tensorboard, and other useful things https://stable-baselines3.readthedocs.io/en/master/guide/tensorboard.html
-# TODO: add checkpointing (every n steps, best, and final)
 
-
-def train_env_supersuit(steps: int = 100_000, seed: int | None = 0, **env_kwargs):
+def train_env_supersuit(steps: int = 1_000_000, seed: int | None = 0, **env_kwargs):
     """Train an agent using the Parallel API."""
 
     env = tank_game_environment_v0.parallel_env_fn(**env_kwargs)
