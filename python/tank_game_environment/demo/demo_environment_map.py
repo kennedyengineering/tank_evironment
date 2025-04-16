132f699dc0b17dd023496c33167fb4fc4c70ed47
Ubuntu <ubuntu@TOOTHLESS.>
2025-04-16 04:04:52 +0000

    added map example to demos

---
+++ python/tank_game_environment/demo/demo_environment_map.py
@@ -0,0 +1,13 @@
+# Tank Game (@kennedyengineering)
+
+from .. import tank_game_environment_v0
+
+env = tank_game_environment_v0.parallel_env_fn(render_mode="human", map_id="Random")
+observations, infos = env.reset()
+
+while env.agents:
+    actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+
+    observations, rewards, terminations, truncations, infos = env.step(actions)
+
+env.close()
