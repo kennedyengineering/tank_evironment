63754801616a4dc2a27df7fec4fe56795696106b
Ubuntu <ubuntu@TOOTHLESS.>
2025-05-20 23:04:47 +0000

    added pdf option to extract_tensorboard

--- python/extract_tensorboard.py
+++ python/extract_tensorboard.py
@@ -96,6 +96,33 @@ def save_scalar_png(reader, output_path):
         plt.savefig(filepath)
 
 
+def save_scalar_pdf(reader, output_path):
+
+    # Make sure output directory exists
+    os.makedirs(output_path, exist_ok=True)
+
+    # Save scalars to plots
+    df = reader.scalars
+    for tag in df["tag"].unique():
+        sub = df[df["tag"] == tag]
+
+        # Create figure
+        plt.figure()
+        plt.plot(sub["step"], sub["value"])
+        plt.title(tag)
+        plt.xlabel("Step")
+        plt.ylabel("Value")
+        plt.grid(True)  # optional, but helpful
+
+        # Build filepath
+        safe_tag = tag.replace("/", "_").replace(" ", "_")
+        filename = f"{safe_tag}.pdf"
+        filepath = os.path.join(output_path, filename)
+
+        # Save plot
+        plt.savefig(filepath)
+
+
 if __name__ == "__main__":
     parser = parser = argparse.ArgumentParser(
         description="Extract and save TensorBoard logged data."
@@ -113,3 +140,4 @@ if __name__ == "__main__":
     save_scalar_csv(reader, os.path.join(args.out_dir, "scalar_csv"))
     save_scalar_pkl(reader, os.path.join(args.out_dir, "scalar_pkl"))
     save_scalar_png(reader, os.path.join(args.out_dir, "scalar_png"))
+    save_scalar_pdf(reader, os.path.join(args.out_dir, "scalar_pdf"))
